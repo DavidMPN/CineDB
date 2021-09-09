731989527125e3eb17312c792d2093d66b50325b
DavidMPN <david.manuel27@hotmail.com>
2021-09-09 13:15:22 -0300

    feat: add page list

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,43 @@
-import styles from '../styles/navbar.css'
+import React from 'react'
 
-function NavBar(){
-    return (
-        <nav className = "navbar">
-          
-        </nav>
-    )
+import '../styles/navbar.css'
+import SearchIcon from '@material-ui/icons/Search';
 
+class NavBar extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <nav class = "navbar">
+                <div class = "nav_left">
+                    <h1 class = "nav_title">
+                        CineDB
+                    </h1>
+                </div>
+
+                <div class = "nav_center">
+                    <ul class = "nav_list">
+                        <li>Filmes</li>
+                        <li>Programação</li>
+                        <li>Trailer</li>
+                        <li>Noticias</li>
+                        <li>Series</li>
+                    </ul> 
+                    <div class = "nav_searchBox">
+                        <input class = "nav_searchBox_input" placeholder = "Pesquisar Filmes ou Series"></input>
+                        <SearchIcon/>
+                    </div>                   
+                </div>
+
+                <div class = "nav_right"></div>
+
+        
+            
+            </nav>
+        ) 
+    }
 }
 
-export default NavBar;
+export default NavBar
